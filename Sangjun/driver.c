c3b79a6bfde2d4be96d30d8abed1ccb60af95d77
5angjun <best_collin@naver.com>
2025-04-21 15:34:26 +0900

    Add wdf

--- Sangjun/driver.c
+++ Sangjun/driver.c
@@ -1,140 +1,186 @@
-/*
- * Copyright 2019 Sergej Schumilo, Cornelius Aschermann
- *
- * SPDX-License-Identifier: MIT
- */
+#include <ntddk.h>
 
- #include <ntddk.h>
+#define DEVICE_NAME         L"\\Device\\ExampleDevice"
+#define SYMLINK_NAME        L"\\DosDevices\\ExampleDevice"
 
- #define DEVICE_NAME         L"\\Device\\testKafl"
- #define DOS_DEVICE_NAME     L"\\DosDevices\\testKafl"
- #define IOCTL_KAFL_INPUT    (ULONG) CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
- 
- NTSTATUS crashMe(IN PIO_STACK_LOCATION IrpStack){
-     SIZE_T size = 0;
-     PCHAR userBuffer = NULL;
- 
-     userBuffer = IrpStack->Parameters.DeviceIoControl.Type3InputBuffer;
-     size = IrpStack->Parameters.DeviceIoControl.InputBufferLength;
- 
-     if (size < 0xe){
-         return STATUS_SUCCESS;
-     }
- 
-     if (userBuffer[0] == 'P'){
-         DbgPrint("[+] KAFL vuln drv -- P");
-         if (userBuffer[1] == 'w'){
-             DbgPrint("[+] KAFL vuln drv -- Pw");
-             if (userBuffer[2] == 'n'){
-                 DbgPrint("[+] KAFL vuln drv -- Pwn");
-                 if (userBuffer[3] == 'T'){
-                     DbgPrint("[+] KAFL vuln drv -- PwnT");
-                     if (userBuffer[4] == 'o'){
-                         DbgPrint("[+] KAFL vuln drv -- PwnTo");
-                         if (userBuffer[5] == 'w'){
-                             DbgPrint("[+] KAFL vuln drv -- PwnTow");
-                             if (userBuffer[6] == 'n'){
-                                 DbgPrint("[+] KAFL vuln drv -- PwnTown: CRASH");
-                                 ((VOID(*)())0x0)();
-                             }
-                         }
-                     }
-                 }
-             }
-         }
-     }
- 
-     if (userBuffer[0] == 'w'){
-         DbgPrint("[+] KAFL vuln drv -- w");
-         if (userBuffer[1] == '0'){
-             DbgPrint("[+] KAFL vuln drv -- w0");
-             if (userBuffer[2] == '0'){
-                 DbgPrint("[+] KAFL vuln drv -- w00");
-                 if (userBuffer[3] == 't'){
-                     DbgPrint("[+] KAFL vuln drv -- w00t: CRASH");
-                     size = *((PSIZE_T)(0x0));
-                 }
-             }
-         }
-     }
- 
-     return STATUS_SUCCESS;
- }
- 
- 
- NTSTATUS handleIrp(IN PDEVICE_OBJECT DeviceObj, IN PIRP pIrp){
-     PIO_STACK_LOCATION	irpStack = 0;
-     ULONG				ioctl;
- 
-     irpStack = IoGetCurrentIrpStackLocation(pIrp);
- 
-     pIrp->IoStatus.Information = 0;
-     pIrp->IoStatus.Status = STATUS_NOT_SUPPORTED;
- 
-     UNREFERENCED_PARAMETER(DeviceObj);
-     PAGED_CODE();
- 
-     switch(irpStack->MajorFunction){
-         case IRP_MJ_DEVICE_CONTROL:
-             ioctl = irpStack->Parameters.DeviceIoControl.IoControlCode;
-             switch(ioctl){
-                 case IOCTL_KAFL_INPUT:
-                     DbgPrint("[+] KAFL vuln drv -- crash attempt\n");
-                     pIrp->IoStatus.Status = crashMe(irpStack);
-                     break;
-                 default:
-                     pIrp->IoStatus.Status = STATUS_INVALID_PARAMETER;
-                     break;
-             }
-             break;
-         case IRP_MJ_CREATE:
-         case IRP_MJ_CLOSE:
-             pIrp->IoStatus.Status = STATUS_SUCCESS;
-             break;
-     };
- 
-     IoCompleteRequest(pIrp, IO_NO_INCREMENT);
-     return pIrp->IoStatus.Status;
- }
- 
- 
- void DriverUnload(PDRIVER_OBJECT pDriverObject){
-     UNICODE_STRING dosDeviceName = { 0 };
-     RtlInitUnicodeString(&dosDeviceName, DOS_DEVICE_NAME);
-     IoDeleteSymbolicLink(&dosDeviceName);
-     IoDeleteDevice(pDriverObject->DeviceObject);
-     DbgPrint("[+] KAFL vuln drv -- unloaded");
- }
- 
- 
- NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObj, IN PUNICODE_STRING RegPath){
-     UINT32          i = 0;
-     NTSTATUS        ntstatus;
-     PDEVICE_OBJECT  deviceObject = NULL;
-     UNICODE_STRING  deviceName, dosDeviceName = { 0 };
- 
-     UNREFERENCED_PARAMETER(RegPath);
-     PAGED_CODE();
- 
-     RtlInitUnicodeString(&deviceName, DEVICE_NAME);
-     RtlInitUnicodeString(&dosDeviceName, DOS_DEVICE_NAME);
- 
-     ntstatus = IoCreateDevice(DriverObj, 0, &deviceName, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &deviceObject);
-     if(!NT_SUCCESS(ntstatus)){
-         DbgPrint("[-] KAFL vuln drv -- IoCreateDevice failed: 0x%X\n", ntstatus);
-         IoDeleteDevice(DriverObj->DeviceObject);
-         return ntstatus;
-     }
- 
-     for (i = 0; i <= IRP_MJ_MAXIMUM_FUNCTION; i++){
-         DriverObj->MajorFunction[i] = handleIrp;
-     }
- 
-     DriverObj->DriverUnload = DriverUnload;
-     deviceObject->Flags |= DO_DIRECT_IO;
-     deviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
-     ntstatus = IoCreateSymbolicLink(&dosDeviceName, &deviceName);
- 
-     DbgPrint("[+] KAFL vuln drv -- loaded");
-     return ntstatus;
- }
+#define IOCTL_ONE           CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_TWO           CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_THREE         CTL_CODE(FILE_DEVICE_UNKNOWN, 0x802, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FOUR          CTL_CODE(FILE_DEVICE_UNKNOWN, 0x803, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FIVE          CTL_CODE(FILE_DEVICE_UNKNOWN, 0x804, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_SIX           CTL_CODE(FILE_DEVICE_UNKNOWN, 0x805, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_SEVEN         CTL_CODE(FILE_DEVICE_UNKNOWN, 0x806, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_EIGHT         CTL_CODE(FILE_DEVICE_UNKNOWN, 0x807, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_NINE          CTL_CODE(FILE_DEVICE_UNKNOWN, 0x808, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_TEN           CTL_CODE(FILE_DEVICE_UNKNOWN, 0x809, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_ELEVEN        CTL_CODE(FILE_DEVICE_UNKNOWN, 0x80A, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+NTSTATUS DispatchDeviceControl(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+    UNREFERENCED_PARAMETER(DeviceObject);
+
+    PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
+    ULONG code = stack->Parameters.DeviceIoControl.IoControlCode;
+    ULONG inLen = stack->Parameters.DeviceIoControl.InputBufferLength;
+    ULONG outLen = stack->Parameters.DeviceIoControl.OutputBufferLength;
+    PUCHAR buffer = (PUCHAR)Irp->AssociatedIrp.SystemBuffer;
+
+    Irp->IoStatus.Information = 0;
+    Irp->IoStatus.Status = STATUS_INVALID_DEVICE_REQUEST;
+
+    switch (code)
+    {
+    case IOCTL_ONE:
+        if (inLen >= 4) {
+            DbgPrint("[IOCTL_ONE] OK\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        }
+        break;
+
+    case IOCTL_TWO:
+        if (outLen == 0x10) {
+            DbgPrint("[IOCTL_TWO] OK\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_BUFFER_TOO_SMALL;
+        }
+        break;
+
+    case IOCTL_THREE:
+        if (inLen == 8 && outLen == 8) {
+            DbgPrint("[IOCTL_THREE] Perfect match\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_INVALID_BUFFER_SIZE;
+        }
+        break;
+
+    case IOCTL_FOUR:
+        if (inLen >= 0x20 && outLen >= 0x20) {
+            DbgPrint("[IOCTL_FOUR] Large buffers\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_BUFFER_TOO_SMALL;
+        }
+        break;
+
+    case IOCTL_FIVE:
+        if (inLen == 1 && buffer[0] == 'A') {
+            DbgPrint("[IOCTL_FIVE] Match A\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    case IOCTL_SIX:
+        if (inLen >= 2 && buffer[0] == 'B' && buffer[1] == 'C') {
+            DbgPrint("[IOCTL_SIX] Starts with BC\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    case IOCTL_SEVEN:
+        if (outLen > 0x30) {
+            DbgPrint("[IOCTL_SEVEN] Big output\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_BUFFER_TOO_SMALL;
+        }
+        break;
+
+    case IOCTL_EIGHT:
+        if (inLen == 0x10 && outLen == 0x40) {
+            DbgPrint("[IOCTL_EIGHT] Complex match\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    case IOCTL_NINE:
+        if (inLen >= 4 && buffer[3] == 'Z') {
+            DbgPrint("[IOCTL_NINE] Z at offset 3\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    case IOCTL_TEN:
+        DbgPrint("[IOCTL_TEN] Default OK\n");
+        Irp->IoStatus.Status = STATUS_SUCCESS;
+        break;
+
+    case IOCTL_ELEVEN:
+        if (inLen >= 2 && buffer[0] == 'X' && buffer[1] == 'Y') {
+            DbgPrint("[IOCTL_ELEVEN] XY match\n");
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+        } else {
+            Irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    default:
+        DbgPrint("[UNKNOWN IOCTL] Not handled\n");
+        Irp->IoStatus.Status = STATUS_INVALID_DEVICE_REQUEST;
+        break;
+    }
+
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+    return Irp->IoStatus.Status;
+}
+
+NTSTATUS DispatchCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+    UNREFERENCED_PARAMETER(DeviceObject);
+    Irp->IoStatus.Status = STATUS_SUCCESS;
+    Irp->IoStatus.Information = 0;
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+    return STATUS_SUCCESS;
+}
+
+void DriverUnload(PDRIVER_OBJECT DriverObject)
+{
+    UNICODE_STRING symlink = RTL_CONSTANT_STRING(SYMLINK_NAME);
+    IoDeleteSymbolicLink(&symlink);
+    IoDeleteDevice(DriverObject->DeviceObject);
+    DbgPrint("[Driver] Unloaded\n");
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+    UNICODE_STRING deviceName = RTL_CONSTANT_STRING(DEVICE_NAME);
+    UNICODE_STRING symlink = RTL_CONSTANT_STRING(SYMLINK_NAME);
+    PDEVICE_OBJECT deviceObject = NULL;
+
+    NTSTATUS status = IoCreateDevice(
+        DriverObject,
+        0,
+        &deviceName,
+        FILE_DEVICE_UNKNOWN,
+        0,
+        FALSE,
+        &deviceObject
+    );
+
+    if (!NT_SUCCESS(status)) {
+        return status;
+    }
+
+    IoCreateSymbolicLink(&symlink, &deviceName);
+
+    for (int i = 0; i <= IRP_MJ_MAXIMUM_FUNCTION; i++) {
+        DriverObject->MajorFunction[i] = DispatchCreateClose;
+    }
+
+    DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DispatchDeviceControl;
+    DriverObject->DriverUnload = DriverUnload;
+
+    DbgPrint("[Driver] Loaded\n");
+    return STATUS_SUCCESS;
+}
--- Sangjun_wdf/driver_wdf.c
+++ Sangjun_wdf/driver_wdf.c
@@ -1,23 +1,186 @@
 #include <ntddk.h>
 #include <wdf.h>
 
+#define DEVICE_NAME         L"\\Device\\ExampleDevice"
+#define SYMLINK_NAME        L"\\DosDevices\\ExampleDevice"
+
+#define IOCTL_ONE           CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_TWO           CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_THREE         CTL_CODE(FILE_DEVICE_UNKNOWN, 0x802, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FOUR          CTL_CODE(FILE_DEVICE_UNKNOWN, 0x803, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FIVE          CTL_CODE(FILE_DEVICE_UNKNOWN, 0x804, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_SIX           CTL_CODE(FILE_DEVICE_UNKNOWN, 0x805, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_SEVEN         CTL_CODE(FILE_DEVICE_UNKNOWN, 0x806, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_EIGHT         CTL_CODE(FILE_DEVICE_UNKNOWN, 0x807, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_NINE          CTL_CODE(FILE_DEVICE_UNKNOWN, 0x808, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_TEN           CTL_CODE(FILE_DEVICE_UNKNOWN, 0x809, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_ELEVEN        CTL_CODE(FILE_DEVICE_UNKNOWN, 0x80A, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
 DRIVER_INITIALIZE DriverEntry;
 EVT_WDF_DRIVER_DEVICE_ADD EvtDeviceAdd;
+EVT_WDF_IO_QUEUE_IO_DEVICE_CONTROL EvtIoDeviceControl;
 EVT_WDF_OBJECT_CONTEXT_CLEANUP EvtDriverContextCleanup;
 
-NTSTATUS EvtDeviceAdd(WDFDRIVER Driver, PWDFDEVICE_INIT DeviceInit) {
+VOID
+EvtIoDeviceControl(
+    WDFQUEUE Queue,
+    WDFREQUEST Request,
+    size_t OutputBufferLength,
+    size_t InputBufferLength,
+    ULONG IoControlCode
+)
+{
+    UNREFERENCED_PARAMETER(Queue);
+    PUCHAR buffer = NULL;
+    NTSTATUS status = STATUS_INVALID_DEVICE_REQUEST;
+
+    WdfRequestRetrieveInputBuffer(Request, 0, (PVOID*)&buffer, NULL);
+
+    switch (IoControlCode) {
+    case IOCTL_ONE:
+        if (InputBufferLength >= 4) {
+            KdPrint(("WDF: IOCTL_ONE OK\n"));
+            status = STATUS_SUCCESS;
+        }
+        break;
+
+    case IOCTL_TWO:
+        if (OutputBufferLength == 0x10) {
+            KdPrint(("WDF: IOCTL_TWO OK\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_BUFFER_TOO_SMALL;
+        }
+        break;
+
+    case IOCTL_THREE:
+        if (InputBufferLength == 8 && OutputBufferLength == 8) {
+            KdPrint(("WDF: IOCTL_THREE Perfect match\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_INVALID_BUFFER_SIZE;
+        }
+        break;
+
+    case IOCTL_FOUR:
+        if (InputBufferLength >= 0x20 && OutputBufferLength >= 0x20) {
+            KdPrint(("WDF: IOCTL_FOUR Large buffers\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_BUFFER_TOO_SMALL;
+        }
+        break;
+
+    case IOCTL_FIVE:
+        if (InputBufferLength == 1 && buffer[0] == 'A') {
+            KdPrint(("WDF: IOCTL_FIVE Match A\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    case IOCTL_SIX:
+        if (InputBufferLength >= 2 && buffer[0] == 'B' && buffer[1] == 'C') {
+            KdPrint(("WDF: IOCTL_SIX Starts with BC\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    case IOCTL_SEVEN:
+        if (OutputBufferLength > 0x30) {
+            KdPrint(("WDF: IOCTL_SEVEN Big output\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_BUFFER_TOO_SMALL;
+        }
+        break;
+
+    case IOCTL_EIGHT:
+        if (InputBufferLength == 0x10 && OutputBufferLength == 0x40) {
+            KdPrint(("WDF: IOCTL_EIGHT Complex match\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    case IOCTL_NINE:
+        if (InputBufferLength >= 4 && buffer[3] == 'Z') {
+            KdPrint(("WDF: IOCTL_NINE Z at offset 3\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    case IOCTL_TEN:
+        KdPrint(("WDF: IOCTL_TEN Default OK\n"));
+        status = STATUS_SUCCESS;
+        break;
+
+    case IOCTL_ELEVEN:
+        if (InputBufferLength >= 2 && buffer[0] == 'X' && buffer[1] == 'Y') {
+            KdPrint(("WDF: IOCTL_ELEVEN XY match\n"));
+            status = STATUS_SUCCESS;
+        } else {
+            status = STATUS_INVALID_PARAMETER;
+        }
+        break;
+
+    default:
+        KdPrint(("WDF: Unknown IOCTL 0x%x\n", IoControlCode));
+        status = STATUS_INVALID_DEVICE_REQUEST;
+        break;
+    }
+
+    WdfRequestComplete(Request, status);
+}
+
+NTSTATUS
+EvtDeviceAdd(
+    WDFDRIVER Driver,
+    PWDFDEVICE_INIT DeviceInit
+)
+{
     UNREFERENCED_PARAMETER(Driver);
-    UNREFERENCED_PARAMETER(DeviceInit);
-    KdPrint(("WDF: DeviceAdd\n"));
-    return STATUS_SUCCESS;
+    NTSTATUS status;
+    WDFDEVICE device;
+    WDF_IO_QUEUE_CONFIG queueConfig;
+    UNICODE_STRING symlink = RTL_CONSTANT_STRING(SYMLINK_NAME);
+
+    WdfDeviceInitSetDeviceType(DeviceInit, FILE_DEVICE_UNKNOWN);
+    status = WdfDeviceCreate(&DeviceInit, WDF_NO_OBJECT_ATTRIBUTES, &device);
+    if (!NT_SUCCESS(status)) {
+        return status;
+    }
+
+    WdfDeviceCreateSymbolicLink(device, &symlink);
+
+    WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&queueConfig, WdfIoQueueDispatchSequential);
+    queueConfig.EvtIoDeviceControl = EvtIoDeviceControl;
+
+    status = WdfIoQueueCreate(device, &queueConfig, WDF_NO_OBJECT_ATTRIBUTES, NULL);
+    return status;
 }
 
-VOID EvtDriverContextCleanup(WDFOBJECT DriverObject) {
+VOID
+EvtDriverContextCleanup(
+    WDFOBJECT DriverObject
+)
+{
     UNREFERENCED_PARAMETER(DriverObject);
-    KdPrint(("WDF: Cleanup\n"));
+    KdPrint(("WDF: Driver cleanup\n"));
 }
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+NTSTATUS
+DriverEntry(
+    PDRIVER_OBJECT DriverObject,
+    PUNICODE_STRING RegistryPath
+)
+{
     WDF_DRIVER_CONFIG config;
     NTSTATUS status;
 
@@ -25,7 +188,6 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) 
     config.EvtDriverUnload = NULL;
 
     status = WdfDriverCreate(DriverObject, RegistryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
-
     if (!NT_SUCCESS(status)) {
         KdPrint(("WDF: DriverEntry failed 0x%x\n", status));
     }
